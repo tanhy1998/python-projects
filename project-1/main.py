eb388f27f630cfd77bafde53d617584acfdd583e
tanhy1998 <tanhongyang0206@gmaail.com>
2026-04-22 23:23:06 +0800

    Refactor PDF generation logic for improved structure and readability

--- project-1/main.py
+++ project-1/main.py
@@ -6,10 +6,12 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
-    pdf.add_page()
+        pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+        pdf.line(10,22,200,22)
 
-    pdf.set_font(family="Times", style="B", size=12)
-    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,22,200,22)
+        for i in range(int(row["Pages"]) - 1):
+            pdf.add_page()
 
 pdf.output("output.pdf")
