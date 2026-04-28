506a661f8f6a6ee139f5a9f2ccb7da9d29e6a3fd
tanhy1998 <tanhongyang0206@gmaail.com>
2026-04-28 22:45:35 +0800

    Enhance PDF footer formatting for improved consistency and readability

--- project-1/main.py
+++ project-1/main.py
@@ -11,7 +11,19 @@ for index, row in df.iterrows():
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
         pdf.line(10,22,200,22)
 
+        # Set the footer
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
         for i in range(int(row["Pages"]) - 1):
             pdf.add_page()
+        
+            pdf.ln(265)
+            pdf.set_font(family="Times", style="I", size=8)
+            pdf.set_text_color(180,180,180)
+            pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
+        
 pdf.output("output.pdf")
